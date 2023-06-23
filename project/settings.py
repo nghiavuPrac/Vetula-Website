fa6c56fd3ee63efa628e80cbc10b1637d22bcfb0
nghiavuPrac <nghiavutrong1998@gmail.com>
2023-06-23 09:57:01 +0700

    fix loading .env

--- project/settings.py
+++ project/settings.py
@@ -1,9 +1,14 @@
-
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.getcwd(),".env")
+print(dotenv_path)
+load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
